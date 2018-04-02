54e7233072851088c458b7f3a738c5dd4dfb0df7
jemesmain <jeaneric.mesmain@gmail.com>
2018-04-02 13:31:39 +0200

    Initial commit

---
+++ app/src/main/java/fr/bicyclopresto/impactdeep/Fragment_setting.java
@@ -0,0 +1,29 @@
+package fr.bicyclopresto.impactdeep;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Fragment_setting extends Fragment {
+
+
+    public Fragment_setting() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_setting, container, false);
+    }
+
+}
